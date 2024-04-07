ed0f7c2c9589af45ad351f3c3ccf91d72c7f747e
rpatetti <patetti.rafael@gmail.com>
2024-04-07 15:52:28 -0300

    Clone Disney Finalizado

--- src/main.js
+++ src/main.js
@@ -1,30 +1,61 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementoDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+function ocultaElementoDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function(event) { // Renomeei 'botao' para 'event'
+        buttons[i].addEventListener('click', function(event) {
             const abaAlvo = event.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`); // Adicionei aspas duplas ao redor do valor do atributo
+            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
             escondeTodosAbas();
-            aba.classList.add('shows__list--is-active'); 
+            aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
             event.target.classList.add('shows__tabs__button--is-active');
         })
     }
-})
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', function(event) {
+            const element = event.currentTarget;
+            const parent = element.closest('.faq__questions__item');
+            parent.classList.toggle('faq__questions__item--is-open');
+        });
+    }
+});
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('shows__tabs__button--is-active');
-    }
+    buttons.forEach(button => {
+        button.classList.remove('shows__tabs__button--is-active');
+    });
 }
 
 function escondeTodosAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
-
-    for (let i = 0; i < tabsContainer.length; i++) {
-        tabsContainer[i].classList.remove('shows__list--is-active');
-    }
+    tabsContainer.forEach(tab => {
+        tab.classList.remove('shows__list--is-active');
+    });
 }
